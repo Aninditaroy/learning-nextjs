6cbb3ebc294c2a75a1d0eab898fd0c146304c531
Aninditaroy <anindita.roy.mu@gmail.com>
2022-07-11 22:38:53 +0600

    added css supports(sass)

--- next_app/pages/posts/first-post.js
+++ next_app/pages/posts/first-post.js
@@ -2,6 +2,7 @@ import Head from 'next/head';
 import Script from 'next/script';
 import Image from 'next/image';
 import nextjs from '../../public/images/nextjs.png';
+import styles from '../../styles/sass/style.module.scss';
 export default function FirstPost() {
     return (
         <>
@@ -11,10 +12,21 @@ export default function FirstPost() {
             <Script>
 
             </Script>
-            <h1>First blog post</h1>
-            {/* <Image src={nextjs}></Image> */}
+            <h1 className={styles.title}>First blog post</h1>
+            {/* <h1 className='title'>First blog post</h1>
+            <style jsx>
+                {
+                    `
+                    .title {
+                        font-size: 129px;
+                        color: #654ff2;
+                    }
+                    `
+                }
+            </style> */}
+            <Image src={nextjs}></Image>
             {/* <Image src='/images/nextjs.png' width={300} height={300}></Image> */}
-            <Image src='/images/nextjs.png' layout='fill'></Image>
+            {/* <Image src='/images/nextjs.png' layout='fill'></Image> */}
         </>
     )
 }
