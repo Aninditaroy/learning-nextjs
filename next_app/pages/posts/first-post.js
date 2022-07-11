3153685de6bbaafdadb04fd221f5ff699c273c14
Aninditaroy <anindita.roy.mu@gmail.com>
2022-07-11 21:27:23 +0600

    worked eith assets and how to use them

--- next_app/pages/posts/first-post.js
+++ next_app/pages/posts/first-post.js
@@ -1,12 +1,20 @@
-import Head from 'next/head'
-
+import Head from 'next/head';
+import Script from 'next/script';
+import Image from 'next/image';
+import nextjs from '../../public/images/nextjs.png';
 export default function FirstPost() {
     return (
         <>
             <Head>
-                <title>NextJS First  Post</title>
+                <title>NextJS First Post</title>
             </Head>
+            <Script>
+
+            </Script>
             <h1>First blog post</h1>
+            {/* <Image src={nextjs}></Image> */}
+            {/* <Image src='/images/nextjs.png' width={300} height={300}></Image> */}
+            <Image src='/images/nextjs.png' layout='fill'></Image>
         </>
     )
 }
